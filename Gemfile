fc3455339f68fc22c6398a4ba455755d6a5af09a
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-30 13:09:24 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 gem 'pry'
 gem 'nokogiri', '1.10'
 gem 'rspec'
+gem 'httparty'
+gem 'byebug'
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,22 @@
 require 'open-uri'
 require 'pry'
-
+require 'httparty'
+require 'byebug'
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
+     html = open(index_url)
+     doc = Nokogiri::HTML(html)
+     roster_body = doc.css('.roster-cards-container')
+     roster_student = doc.css('.student-card')
+     roster_student.each do |kay,value|
+        byebug
+     end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
